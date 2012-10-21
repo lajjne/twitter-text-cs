c50e432fab0a94e7c7e23b9b6e64b59472d5c898
Linus Birgerstam <linus@birgerstam.se>
2012-10-21 12:18:21 +0200

    More work on loading test cases from YAML

--- Tests/TwitterTextTests.cs
+++ Tests/TwitterTextTests.cs
@@ -5,68 +5,33 @@ using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using YamlDotNet.RepresentationModel;
 using TwitterText;
+using System.Dynamic;
 
 namespace Tests {
 
-    public class TestData {
-        public string Description { get; set; }
-        public string Text { get; set; }
-        public List<string> Expected { get; set; }
-        public List<string> Actual { get; set; }
-    }
-
     [TestClass]
     public class TwitterTextTests {
-        private Dictionary<string, Dictionary<string, List<TestData>>> _testdata = new Dictionary<string, Dictionary<string, List<TestData>>>();
+        private Dictionary<string, Dictionary<string, List<dynamic>>> _testdata = new Dictionary<string, Dictionary<string, List<dynamic>>>();
 
         public TestContext TestContext { get; set; }
 
-        public IList<TestData> LoadTests(string file, string section) {
-            Dictionary<string, List<TestData>> dict = null;
-
-            if (!_testdata.TryGetValue(file, out dict)) {
-
-                // load YAML file
-                var stream = new StreamReader(Path.Combine(TestContext.TestDeploymentDir, file));
-                var yaml = new YamlStream();
-                yaml.Load(stream);
-
-                // load tests
-                var root = yaml.Documents[0].RootNode as YamlMappingNode;
-                var tests = root.Children[new YamlScalarNode("tests")] as YamlMappingNode;
-                dict = new Dictionary<string, List<TestData>>();
-                foreach (var entry in tests.Children) {
-                    var sect = entry.Key as YamlScalarNode;
-                    var items = entry.Value as YamlSequenceNode;
-                    var list = new List<TestData>();
-                    foreach (YamlMappingNode item in items) {
-                        list.Add(new TestData {
-                            Description = item.Children[new YamlScalarNode("description")].ToString(),
-                            Text = item.Children[new YamlScalarNode("text")].ToString(),
-                            Expected = ((YamlSequenceNode)item.Children[new YamlScalarNode("expected")]).Children.ToList().ConvertAll(x => x.ToString())
-                        });
-                    }
-                    dict.Add(sect.Value, list);
-                }
-                _testdata.Add(file, dict);
-            } else {
-                return dict[section];
-            }
-
-            Assert.Fail("File not found: " + file);
-            return null;
-        }
+      
 
         [TestMethod]
         public void ExtractMentionsTest() {
             foreach (var test in LoadTests("extract.yml", "mentions")) {
-                CollectionAssert.AreEquivalent(test.Expected, test.Text.ExtractUsernames(), test.Description);
+                var actual = TweetExtensions.ExtractUsernames(test.Text);
+                CollectionAssert.AreEquivalent(test.Expected, actual, test.Description);
             }
         }
 
         [TestMethod]
         public void ExtractMentionsWithIndicesTest() {
-            Assert.Inconclusive();
+            Extractor extractor = new Extractor();
+            foreach (var test in LoadTests("extract.yml", "mentions_with_indices")) {
+                var actual = extractor.extractMentionedScreennamesWithIndices(test.Text);
+                CollectionAssert.AreEquivalent(test.Expected, actual, test.Description);
+            }
         }
 
         [TestMethod]
@@ -76,7 +41,10 @@ namespace Tests {
 
         [TestMethod]
         public void ExtractRepliesTest() {
-            Assert.Inconclusive();
+            foreach (var test in LoadTests("extract.yml", "replies")) {
+                var actual = ((string)test.Text).ExtractReplyUsername();
+                Assert.AreEqual(test.Expected, actual, test.Description);
+            }
         }
 
         [TestMethod]
@@ -122,6 +90,60 @@ namespace Tests {
         [TestMethod]
         public void ValidateTest() {
             Assert.Inconclusive();
+        }
+
+        private IList<dynamic> LoadTests(string file, string type) {
+            Dictionary<string, List<dynamic>> dict = null;
+            if (!_testdata.TryGetValue(file, out dict)) {
+
+                // load yaml file
+                var stream = new StreamReader(Path.Combine(TestContext.TestDeploymentDir, file));
+                var yaml = new YamlStream();
+                yaml.Load(stream);
+
+                // load tests
+                var root = yaml.Documents[0].RootNode as YamlMappingNode;
+                var tests = root.Children[new YamlScalarNode("tests")] as YamlMappingNode;
+                dict = new Dictionary<string, List<dynamic>>();
+                foreach (var entry in tests.Children) {
+                    var sect = entry.Key as YamlScalarNode;
+                    var items = entry.Value as YamlSequenceNode;
+                    var list = new List<dynamic>();
+                    foreach (YamlMappingNode item in items) {
+                        dynamic test = new ExpandoObject();
+                        test.Description = ConvertNode(item.Children.Single(x => x.Key.ToString() == "description").Value);
+                        test.Text = ConvertNode(item.Children.Single(x => x.Key.ToString() == "text").Value);
+                        test.Expected = ConvertNode(item.Children.Single(x => x.Key.ToString() == "expected").Value);
+                        test.Hits = ConvertNode(item.Children.SingleOrDefault(x => x.Key.ToString() == "hits").Value);
+                        list.Add(test);
+                    }
+                    dict.Add(sect.Value, list);
+                }
+                _testdata.Add(file, dict);
+            }
+
+            return dict[type];
+        }
+
+        private dynamic ConvertNode(YamlNode node) {
+            dynamic dynnode = node as dynamic;
+
+            if (node is YamlScalarNode) {
+                return string.IsNullOrEmpty(dynnode.Value) ? null : dynnode.Value;
+            } else if (node is YamlSequenceNode) {
+                var list = new List<dynamic>();
+                foreach (var item in dynnode.Children) {
+                    list.Add(ConvertNode(item));
+                }
+                return list;
+            } else if (node is YamlMappingNode) {
+                dynamic mapnode = new ExpandoObject();
+                foreach (var item in ((YamlMappingNode)node).Children) {
+                    ((IDictionary<string, object>)mapnode).Add(item.Key.ToString(), ConvertNode(item.Value));
+                }
+                return mapnode;
+            }
+            return null;
         }
 
     }
